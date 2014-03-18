e40a2beef937491c0322a0988ff0ead2305ada55
Jackson Palmer <hello@ummjackson.com>
2014-03-17 20:05:03 -0700

    Just need to do images.

--- js/foundation.js
+++ js/foundation.js
@@ -6,24 +6,9 @@ $(document).ready(function() {
 		  	$('.projects').append('<h3>' + this.name + '</h3>');
 		  	$.each(this.cards, function() {
 		  		$('.projects').append('<img src=' + this.shortUrl + '.png />');
-		  		var start_pos = this.desc.indexOf('[') + 1;
-				var end_pos = this.desc.indexOf(']',start_pos);
-				var address = this.desc.substring(start_pos,end_pos);
-				if (address.charAt(0) == "D") {
-		  		$('.projects').append('<div><span class="label label-success"><span class="lookup" data-address="' + address + '""></span> DOGE</span></div>');
-		  		}
 		  	});
 		  });
 		 });
-
-		$(".lookup").each(function() {
-		address = $(this).data("address");
-		lookup = $(this);
-		$.getJSON("http://query.yahooapis.com/v1/public/yql?q=select%20p%20from%20html%20where%20url%20%3D%20'http%3A%2F%2Fdogechain.info%2Fchain%2FDogecoin%2Fq%2Faddressbalance%2F" + address + "'&format=json", function(data) {
-			$(lookup).text(Math.round(data.query.results.body.p).toLocaleString());
-		});
-		});
-
 	}
 
 	$('#bottomPanel').click(function() {
